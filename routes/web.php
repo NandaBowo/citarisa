3d55254f07538daacbc8a92315c8c0a172a31c91
NandaBowo <nandabowo.nb@gmail.com>
2024-03-30 14:43:48 +0700

    Fix the routes conflict

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HydraController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,16 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Route::get('/hydra', [HydraController::class, 'index'])->name('hydra');
 
 Route::get('/login', [AuthController::class, 'getLogin']);
 Route::post('/login', [AuthController::class, 'postLogin']);
 Route::get('/logout', [AuthController::class, 'logout']);
-=======
 Route::get('/hydra', [App\Http\Controllers\HydraController::class, 'index'])->name('hydra');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
->>>>>>> 6a7960130338b9862df9e3dfdf2569663ce19a10
